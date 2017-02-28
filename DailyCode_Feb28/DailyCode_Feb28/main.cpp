690bdbefdbdcb09a47c7d22a076a5760b60a0bba
game110pm <game110pm@605-110-STU18>
2017-02-28 12:54:34 -0700

    BOW TO THE GOLRY OF RICK ASTLEY

---
+++ DailyCode_Feb28/DailyCode_Feb28/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+void Rick_Roll();
+
+int main() {
+
+	while (1 > 0) {
+		 Rick_Roll();
+}
+
+
+	}
+
+void Rick_Roll() {
+
+	MessageBox(0, "Never gonna give you up...", "RICK_ROLL.exe", 0);
+	MessageBox(0, "Never gonna let you down...", "RICK_ROLL.exe", 0);
+	MessageBox(0, "Never gonna run around, and desert you...", "RICK_ROLL.exe", 0);
+	MessageBox(0, "Never gonna make you cry...", "RICK_ROLL.exe", 0);
+	MessageBox(0, "Never say goodbye...", "RICK_ROLL.exe", 0);
+	MessageBox(0, "Never tell a lie, and hurt you...", "RICK_ROLL.exe", 0);
+
+
+}
